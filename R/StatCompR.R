8cc41531dbf0541cb418a6d48478df3a80fdfdc2
zhanghfd <zhangh@ustc.edu.cn>
2019-12-27 10:42:32 +0800

    first commit

---
+++ R/StatCompR.R
@@ -0,0 +1,71 @@
+#' @title Benchmark R and Rcpp functions.
+#' @name benchmarks
+#' @description Use R package \code{microbenchmark} to compare the performance of C functions (\code{gibbsR} and \code{vaccR}) and Cpp functions (\code{gibbsC} and \code{vaccC}).
+#' @import microbenchmark
+#' @importFrom Rcpp evalCpp
+#' @importFrom stats rnorm rgamma
+#' @useDynLib StatComp
+#' @examples
+#' \dontrun{
+#' data(data)
+#' attach(data)
+#' tm1 <- microbenchmark::microbenchmark(
+#'   rnR = gibbsR(100,10),
+#'   rnC = gibbsC(100,10)
+#' )
+#' print(summary(tm1)[,c(1,3,5,6)])
+#' 
+#' tm2 <- microbenchmark::microbenchmark(
+#'   vR = vaccR(age,female,ily),
+#'   vC = vaccC(age,female,ily)
+#' )
+#' print(summary(tm2)[,c(1,3,5,6)])
+#' }
+NULL
+
+#' @title Use three inputs to predict response using R.
+#' @description The prediction model is described in http://www.babelgraph.org/wp/?p=358.
+#' @param age the first predictor (numeric)
+#' @param female the second predictor (logical)
+#' @param ily the third predictor (logical)
+#' @return a random sample of size \code{n}
+#' @examples
+#' \dontrun{
+#' data(data)
+#' attach(data)
+#' res <- vaccR(age,female,ily)
+#' }
+#' @export
+vaccR <- function(age, female, ily) {
+  p <- 0.25 + 0.3 * 1 / (1 - exp(0.04 * age)) + 0.1 * ily
+  p <- p * ifelse(female, 1.25, 0.75)
+  p <- pmax(0, p)
+  p <- pmin(1, p)
+  p
+}
+
+#' @title A Gibbs sampler using R
+#' @description A Gibbs sampler using R
+#' @param N the number of samples
+#' @param thin the number of between-sample random numbers
+#' @return a random sample of size \code{n}
+#' @examples
+#' \dontrun{
+#' rnR <- gibbsR(100,10)
+#' par(mfrow=c(2,1));
+#' plot(rnR[,1],type='l')
+#' plot(rnR[,2],type='l')
+#' }
+#' @export
+gibbsR <- function(N, thin) {
+  mat <- matrix(nrow = N, ncol = 2)
+  x <- y <- 0
+  for (i in 1:N) {
+    for (j in 1:thin) {
+      x <- rgamma(1, 3, y * y + 4)
+      y <- rnorm(1, 1 / (x + 1), 1 / sqrt(2 * (x + 1)))
+    }
+    mat[i, ] <- c(x, y)
+  }
+  mat
+}
---
+++ src/StatCompC.cpp
@@ -0,0 +1,64 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//' @title A Gibbs sampler using Rcpp
+//' @description A Gibbs sampler using Rcpp
+//' @param N the number of samples
+//' @param thin the number of between-sample random numbers
+//' @return a random sample of size \code{n}
+//' @examples
+//' \dontrun{
+//' rnC <- gibbsC(100,10)
+//' par(mfrow=c(2,1));
+//' plot(rnC[,1],type='l')
+//' plot(rnC[,2],type='l')
+//' }
+//' @export
+// [[Rcpp::export]]
+NumericMatrix gibbsC(int N, int thin) {
+  NumericMatrix mat(N, 2);
+  double x = 0, y = 0;
+  for(int i = 0; i < N; i++) {
+    for(int j = 0; j < thin; j++) {
+      x = rgamma(1, 3, 1 / (y * y + 4))[0];
+      y = rnorm(1, 1 / (x + 1), 1 / sqrt(2 * (x + 1)))[0];
+    }
+    mat(i, 0) = x;
+    mat(i, 1) = y;
+  }
+  return(mat);
+}
+#include <Rcpp.h>
+using namespace Rcpp;
+
+double vacc3a(double age, bool female, bool ily){
+  double p = 0.25 + 0.3 * 1 / (1 - exp(0.04 * age)) + 0.1 * ily;
+  p = p * (female ? 1.25 : 0.75);
+  p = std::max(p, 0.0);
+  p = std::min(p, 1.0);
+  return p;
+}
+
+//' @title Use three inputs to predict response using Rcpp.
+//' @description The prediction model is described in http://www.babelgraph.org/wp/?p=358.
+//' @param age the first predictor (numeric)
+//' @param female the second predictor (logical)
+//' @param ily the third predictor (logical)
+//' @return a random sample of size \code{n}
+//' @examples
+//' \dontrun{
+//' data(data)
+//' attach(data)
+//' res <- vaccC(age,female,ily)
+//' }
+//' @export
+// [[Rcpp::export]]
+NumericVector vaccC(NumericVector age, LogicalVector female,
+                    LogicalVector ily) {
+  int n = age.size();
+  NumericVector out(n);
+  for(int i = 0; i < n; ++i) {
+    out[i] = vacc3a(age[i], female[i], ily[i]);
+  }
+  return out;
+}
