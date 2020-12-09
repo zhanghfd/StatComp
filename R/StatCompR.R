b381a4964698a44a04dd4bc8c0d792129e72d5ae
zhanghfd <zhangh@ustc.edu.cn>
2020-12-09 22:13:14 +0800

    add document for data

--- R/StatCompR.R
+++ R/StatCompR.R
@@ -1,10 +1,21 @@
+#' @title A illustration dataset
+#' @name data
+#' @description A dataset used to illustrate the performance of \code{vaccR} and \code{vaccC}.
+#' @examples
+#' \dontrun{
+#' data(data)
+#' attach(data)
+#' tm <- microbenchmark::microbenchmark(
+#'   vR = vaccR(age,female,ily),
+#'   vC = vaccC(age,female,ily)
+#' )
+#' print(summary(tm)[,c(1,3,5,6)])
+#' }
+NULL
+
 #' @title Benchmark R and Rcpp functions.
 #' @name benchmarks
 #' @description Use R package \code{microbenchmark} to compare the performance of C functions (\code{gibbsR} and \code{vaccR}) and Cpp functions (\code{gibbsC} and \code{vaccC}).
-#' @import microbenchmark
-#' @importFrom Rcpp evalCpp
-#' @importFrom stats rnorm rgamma
-#' @useDynLib StatComp
 #' @examples
 #' \dontrun{
 #' data(data)
@@ -20,6 +31,25 @@
 #'   vC = vaccC(age,female,ily)
 #' )
 #' print(summary(tm2)[,c(1,3,5,6)])
+#' }
+#' @import microbenchmark
+#' @importFrom Rcpp evalCpp
+#' @importFrom stats rnorm rgamma
+#' @useDynLib StatComp
+NULL
+
+#' @title A dataset used for illustration.
+#' @name data
+#' @description This dataset is used to compare the performance of C function \code{vaccR}) and C++ function \code{vaccC}.
+#' @examples
+#' \dontrun{
+#' data(data)
+#' attach(data)
+#' tm <- microbenchmark::microbenchmark(
+#'   vR = vaccR(age,female,ily),
+#'   vC = vaccC(age,female,ily)
+#' )
+#' print(summary(tm)[,c(1,3,5,6)])
 #' }
 NULL
 
